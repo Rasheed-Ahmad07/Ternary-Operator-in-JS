15a1845393c94ad98e1b03a7e9f2ada84c97ed13
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-18 17:24:57 +0500

    Add files via upload

---
+++ 008 ternary operater/index.js
@@ -0,0 +1,16 @@
+//&     ternary operater 
+
+var bilal= false;
+
+// var sadeeq = bilal ? 'abc' : 'xyz';
+// console.log(sadeeq);
+
+
+// var Rain=false;
+
+// var x= Rain ? 'chatrai rawala ' : ' chatrai ma rala'
+// console.log(x);
+let number =11;
+var z = (number % 2 ===0 ) ? 'even' : 'odd'
+console.log(z);
+
